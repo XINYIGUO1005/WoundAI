98bd3c81dffee365bb806977d5dc19a94542c2e2
XINYIGUO1005 <275223940@QQ.COM>
2026-06-18 15:51:33 +0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -6,15 +6,14 @@ def get_scratch_mask(img):
 
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    # 平滑
     gray = cv2.GaussianBlur(gray, (9, 9), 0)
 
     h, w = gray.shape
 
-    # 计算每一列的纹理（标准差）
+    # 每一列灰度标准差
     profile = np.std(gray, axis=0)
 
-    # 划痕区纹理少，因此标准差低
+    # 平滑曲线
     smooth_profile = cv2.GaussianBlur(
         profile.reshape(1, -1),
         (1, 31),
@@ -25,12 +24,10 @@ def get_scratch_mask(img):
     center = np.argmin(smooth_profile)
 
     # 最低值附近20%
-作为划痕
     threshold = smooth_profile[center] * 1.2
 
     scratch_cols = smooth_profile <= threshold
 
-    # 找包含中心的连续区域
     left = center
     while left > 0 and scratch_cols[left]:
         left -= 1
@@ -67,6 +64,7 @@ def make_overlay(img, mask):
     overlay = img.copy()
 
     red = np.zeros_like(img)
+
     red[:, :, 0] = 255
 
     alpha = 0.3
