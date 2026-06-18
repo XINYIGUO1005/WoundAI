afb0f387ba1840460354ae255bf871a899d11b0a
XINYIGUO1005 <275223940@QQ.COM>
2026-06-18 15:58:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,7 @@ from analysis import (
 from plot import draw_barplot
 
 
-st.set_page_config(
-    page_title="WoundAI",
-    layout="wide"
-)
+st.set_page_config(page_title="WoundAI", layout="wide")
 
 st.title("🔬 WoundAI")
 st.subheader("Cell Migration Analyzer")
@@ -32,149 +29,81 @@ if uploaded_files:
 
     pairs = {}
 
+    # -------- grouping --------
     for file in uploaded_files:
-
         name = file.name
 
         if "0h" in name.lower():
-
-            sample = re.sub(
-                r'[-_]?0h.*',
-                '',
-                name,
-                flags=re.I
-            )
-
-            if sample not in pairs:
-                pairs[sample] = {}
-
-            pairs[sample]["0h"] = file
+            sample = re.sub(r'[-_]?0h.*', '', name, flags=re.I)
+            pairs.setdefault(sample, {})["0h"] = file
 
         elif "24h" in name.lower():
-
-            sample = re.sub(
-                r'[-_]?24h.*',
-                '',
-                name,
-                flags=re.I
-            )
-
-            if sample not in pairs:
-                pairs[sample] = {}
-
-            pairs[sample]["24h"] = file
+            sample = re.sub(r'[-_]?24h.*', '', name, flags=re.I)
+            pairs.setdefault(sample, {})["24h"] = file
 
     results = []
 
+    # -------- per sample analysis --------
     for sample, imgs in pairs.items():
 
-        if "0h" in imgs and "24h" in imgs:
+        if "0h" not in imgs or "24h" not in imgs:
+            continue
 
-            img0 = np.array(
-                Image.open(
-                    imgs["0h"]
-                ).convert("RGB")
-            )
+        img0 = np.array(Image.open(imgs["0h"]).convert("RGB"))
+        img24 = np.array(Image.open(imgs["24h"]).convert("RGB"))
 
-            img24 = np.array(
-                Image.open(
-                    imgs["24h"]
-                ).convert("RGB")
-            )
+        mask0 = get_scratch_mask(img0)
+        mask24 = get_scratch_mask(img24)
 
-            mask0 = get_scratch_mask(img0)
-            mask24 = get_scratch_mask(img24)
+        area0 = get_area(mask0)
+        area24 = get_area(mask24)
 
-            area0 = get_area(mask0)
-            area24 = get_area(mask24)
+        migration = calculate_migration(area0, area24)
 
-            migration = calculate_migration(
-                area0,
-                area24
-            )
+        results.append({
+            "Sample": sample,
+            "Area_0h": area0,
+            "Area_24h": area24,
+            "Migration_%": migration
+        })
 
-            results.append(
-                {
-                    "Sample": sample,
-                    "Area_0h": area0,
-                    "Area_24h": area24,
-                    "Migration_%": migration
-                }
-            )
+        overlay0 = make_overlay(img0, mask0)
+        overlay24 = make_overlay(img24, mask24)
 
-            overlay0 = make_overlay(
-                img0,
-                mask0
-            )
+        st.write("---")
+        st.subheader(sample)
 
-            overlay24 = make_overlay(
-                img24,
-                mask24
-            )
-
-            st.write("---")
-            st.subheader(sample)
-
+        # -------- display per sample --------
         st.write("### 0h")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.image(img0, caption="Original")
+        with col2:
+            st.image(mask0, caption="Mask")
+        with col3:
+            st.image(overlay0, caption="Overlay")
 
-col1, col2, col3 = st.columns(3)
+        st.write("### 24h")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.image(img24, caption="Original")
+        with col2:
+            st.image(mask24, caption="Mask")
+        with col3:
+            st.image(overlay24, caption="Overlay")
 
-with col1:
-    st.image(
-        img0,
-        caption="Original"
-    )
-
-with col2:
-    st.image(
-        mask0,
-        caption="Mask"
-    )
-
-with col3:
-    st.image(
-        overlay0,
-        caption="Overlay"
-    )
-
-st.write("### 24h")
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.image(
-        img24,
-        caption="Original"
-    )
-
-with col2:
-    st.image(
-        mask24,
-        caption="Mask"
-    )
-
-with col3:
-    st.image(
-        overlay24,
-        caption="Overlay"
-    )
-
-    if len(results) > 0:
+    # -------- summary section --------
+    if results:
 
         df = pd.DataFrame(results)
 
         st.write("## Results")
-
         st.dataframe(df)
 
         fig = draw_barplot(df)
-
         st.pyplot(fig)
 
-        csv = df.to_csv(
-            index=False
-        )
-
+        csv = df.to_csv(index=False)
         st.download_button(
             "Download CSV",
             csv,
@@ -183,7 +112,4 @@ with col3:
         )
 
 else:
-
-    st.info(
-        "Please upload images."
-    )
+    st.info("Please upload images.")
